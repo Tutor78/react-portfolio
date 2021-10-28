11e194e7cd674c655cce96990b362226a23b1e9c
Michael Tutor <tutor.michael78@gmail.com>
2021-10-28 11:19:50 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Nav from './components/Nav';
+import Header from './components/Header';
 
 import './App.css';
 
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className='container'>
-      <Nav></Nav>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Nav from '../Nav';
+
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1>Mike Tutor</h1>
+            <Nav />
+        </div>
+    )
+}
+
+export default Header;
